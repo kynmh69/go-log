0e0204007bbbfd183075297df7f75e15e0b46f33
kynmh69 <hiroki.kayanuma@gmail.com>
2023-05-27 14:59:34 +0900

    初期化 #1 initialize go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module kynmh69/logging
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "log"
+
+var logging log.Logger
+
+func main()  {
+	// Logger initilize.
+	logging = log.Logger{}
+	logging
+	// open env file
+	// Logger output file and std stream.
+
+}
