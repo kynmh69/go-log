c1f684da19bd5ad1ccdd64f5750faa245ebe2801
kynmh69 <hiroki.kayanuma@gmail.com>
2023-05-27 17:03:58 +0900

    初期化 #1 mod logging

--- logging/logger.go
+++ logging/logger.go
@@ -2,25 +2,30 @@ package logging
 
 import (
 	"fmt"
+	"io"
 	"log"
-	"os"
 	"time"
 )
 
 type Logger struct {
-	logger   log.Logger
-	LogLevel Level
+	Logger    []log.Logger
+	LogLevel  Level
+	LogFormat string
 }
 
-func (l *Logger) InitLogger() {
-	l.logger = *log.New(os.Stdout, "", log.Lshortfile)
+func New(streams []io.Writer) *Logger {
+	l := &Logger{}
+	for _, stream := range streams {
+		l.Logger = append(l.Logger, *log.New(stream, "", log.Lshortfile))
+	}
+	return l
 }
 
 func (l *Logger) print(level Level, msg string) {
 	printStr := ""
 	printArr := []string{}
 
-	printArr = append(printArr, l.Now())
+	printArr = append(printArr, l.Now(l.LogFormat))
 	printArr = append(printArr, fmt.Sprintf("[%s]", level.String()))
 	printArr = append(printArr, msg)
 	for i := 0; i < len(printArr); i++ {
@@ -32,7 +37,9 @@ func (l *Logger) print(level Level, msg string) {
 		// スペース追加
 		printStr += " "
 	}
-	l.logger.Println(printStr)
+	for _, stream := range l.Logger {
+		stream.Println(printStr)
+	}
 }
 
 func (l *Logger) Debug(msg string) {
@@ -65,7 +72,12 @@ func (l *Logger) Critical(msg string) {
 	}
 }
 
-func (l *Logger) Now() string {
+func (l *Logger) Now(format string) string {
 	time := time.Now()
-	return time.String()
+
+	if format == "" {
+		format = "2006-01-02 15:04:05"
+	}
+
+	return time.Format(format)
 }
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "kynmh69/logging/logging"
+import (
+	"io"
+	"kynmh69/logging/logging"
+	"os"
+)
 
 func main() {
+	filePath := "./log/"
+	fp := os.OpenFile()
 	// Logger initilize.
-	log := logging.Logger{}
-	log.InitLogger()
+	log := logging.New([]io.Writer{os.Stdout})
 	// set log level
 	level := logging.INFO
 	// open env file
